a262981794751ae28c7a6c52b20ebcaaff67c94b
Vee Satayamas <vsatayamas@gmail.com>
2017-01-23 08:55:22 +0700

    download and unzip

---
+++ download.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl 'http://nlp.stanford.edu/software/stanford-corenlp-full-2016-10-31.zip' -o stanford-corenlp-full-2016-10-31.zip
---
+++ unzip.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+unzip stanford-corenlp-full-2016-10-31.zip
